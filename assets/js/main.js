0008345b8d5fe8a2e3e7d59ca77d8dbc290f27ee
Thiago Diniz <thiagovsdiniz@gmail.com>
2013-05-28 19:36:32 -0300

    small fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,7 @@ $(function(){
     var $window = $(window)
 
     setTimeout(function () {
-      $('.nav-list').affix({
-        offset: {
-          top: function () { return $window.width() <= 980 ? 260 : 210 }
-        , bottom: 300
-        }
-      })
+      $('.nav-list').affix();
     }, 100);
     
   });
